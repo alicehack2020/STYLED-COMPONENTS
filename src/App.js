3d2eed778513a0646434d7a9d8c0df9171981f77
Mangesh2042 <mangesh2042@gmail.com>
2022-06-15 08:26:39 +0530

    styled components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import styled from 'styled-components'
 
-function App() {
+const DivWrapper=styled.div`
+  display: flex;
+  
+`;
+
+const PrimaryButton=styled.button`
+       background-color: skyblue;
+        border: none;
+        padding: 10px;
+        margin: 10px;
+`;
+
+const DefaultButton=styled.button`
+background-color: white;
+border: 1px solid black;
+margin: 10px;
+`
+
+
+const DashedButton=styled.button`
+background-color: white;
+border: 1px dashed black;
+margin: 10px;
+`
+
+const TextButton=styled.button`
+background-color: white;
+border: none;
+margin: 10px;
+`
+
+const LinkButton=styled.button`
+background-color: white;
+border: none;
+color: skyblue;
+margin: 10px;
+`
+
+
+
+
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <DivWrapper>
+          <PrimaryButton>Primary Button</PrimaryButton>
+          <DefaultButton>Default Button</DefaultButton>
+          <DashedButton>Dashed Button</DashedButton>
+          <TextButton>Text Button</TextButton>
+          <LinkButton>Link Button</LinkButton>
+    </DivWrapper>
+  )
 }
 
-export default App;
+export default App
